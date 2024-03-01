bc028c838c4c66ecb4de45de4b0d106c5260b647
Vika Zadorozhnaya <150604407+Vviktori@users.noreply.github.com>
2024-03-01 19:19:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,49 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value > 1) {
+    for (uint64_t i = 2; i <= value / 2; i++) {
+      if (value % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t counter = 0;
+  uint64_t number = 2;
+  for (uint64_t i = 0; i < n; ) {
+    if (checkPrime(number)) {
+      counter++;
+      if (counter == n) {
+        return number;
+      }
+    }
+    number++;
+  }
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t number = value + 1;
+  while (true) {
+    if (checkPrime(number)) {
+      return number;
+    }
+    number++;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+  for (uint64_t i = 0; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum = sum + i;
+    }
+  }
+  return sum;
 }
